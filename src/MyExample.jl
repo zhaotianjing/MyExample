156dc29138e7450a46365edfd4a945b508981809
tj <tjzhao@ucdavis.edu>
2020-09-17 21:37:06 -0700

    test

--- src/MyExample.jl
+++ src/MyExample.jl
@@ -1,5 +1,6 @@
 module MyExample
 
 # Write your package code here.
+include("extra.jl")
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,10 @@
 using MyExample
 using Test
 
+
+my_f(2,3)
+
 @testset "MyExample.jl" begin
     # Write your tests here.
+    my_f(2,3)
 end
